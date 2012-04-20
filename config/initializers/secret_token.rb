f3934b2128fb6ba54d2f5dff834a458f9e0558d0
fakestarbaby <fakestarbaby@gmail.com>
2012-04-20 09:18:59 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleOctokit::Application.config.secret_token = '5dced40037dbcb9adfa483990d0cb4402924eca9b289283d1f2f9ab3a4608c84c8bfb7e6d323fb29c95f5ad1d0451f755f9935871957e0f640ed1d1c056c6100'
